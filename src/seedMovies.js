778f2847eacd6703d8ccd7b22e3046b046ba68d4
Hady Salhab <hadysalhab1@gmail.com>
2020-05-31 21:56:27 +1000

    Add seedMovies.js

---
+++ src/seedMovies.js
@@ -0,0 +1,216 @@
+export default [
+	{
+		paletteName: "Popular Movies",
+		id: "popular-movies",
+		emoji: "🎨",
+		movies: [
+			{
+				poster_path: "/xBHvZcjRiWyobQ9kxBhO6B2dtRI.jpg",
+				id: 419704,
+				title: "Ad Astra",
+			},
+			{
+				poster_path: "/8WUVHemHFH2ZIP6NWkwlHWsyrEL.jpg",
+				id: 338762,
+				title: "Bloodshot",
+			},
+			{
+				poster_path: "/jHo2M1OiH9Re33jYtUQdfzPeUkx.jpg",
+				id: 385103,
+				title: "Scoob!",
+			},
+			{
+				id: 454626,
+				title: "Sonic the Hedgehog",
+				poster_path: "/aQvJ5WPzZgYVDrxLX4R6cLJCEaQ.jpg",
+			},
+			{
+				poster_path: "/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg",
+				id: 38700,
+				title: "Bad Boys for Life",
+			},
+			{
+				poster_path: "/5jdLnvALCpK1NkeQU1z4YvOe2dZ.jpg",
+				id: 576156,
+				title: "The Lovebirds",
+			},
+			{
+				poster_path: "/h4VB6m0RwcicVEZvzftYZyKXs6K.jpg",
+				id: 495764,
+				title: "Birds of Prey",
+			},
+			{
+				poster_path: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+				id: 475557,
+				title: "Joker",
+			},
+			{
+				poster_path: "/f4aul3FyD3jv3v4bul1IrkWZvzq.jpg",
+				id: 508439,
+				title: "Onward",
+			},
+			{
+				poster_path: "/pjeMs3yqRmFL3giJy4PMXWZTTPa.jpg",
+				id: 330457,
+				title: "Frozen II",
+			},
+			{
+				poster_path: "/niyXFhGIk4W2WTcX2Eod8vx2Mfe.jpg",
+				id: 686245,
+				title: "Survive the Night",
+			},
+			{
+				poster_path: "/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+				id: 496243,
+				title: "Parasite",
+			},
+			{
+				poster_path: "/db32LaOibwEliAmSL2jjDF6oDdj.jpg",
+				id: 181812,
+				title: "Star Wars: The Rise of Skywalker",
+			},
+			{
+				poster_path: "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
+				id: 299536,
+				title: "Avengers: Infinity War",
+			},
+			{
+				poster_path: "/qa6HCwP4Z15l3hpsASz3auugEW6.jpg",
+				id: 920,
+				title: "Cars",
+			},
+			{
+				poster_path: "/iZf0KyrE25z1sage4SYFLCCrMi9.jpg",
+				id: 530915,
+				title: "1917",
+			},
+			{
+				poster_path: "/wlfDxbGEsW58vGhFljKkcR5IxDj.jpg",
+				id: 545609,
+				title: "Extraction",
+			},
+			{
+				poster_path: "/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg",
+				id: 420817,
+				title: "Aladdin",
+			},
+			{
+				poster_path: "/qgrk7r1fV4IjuoeiGS5HOhXNdLJ.jpg",
+				id: 568160,
+				title: "Weathering with You",
+			},
+			{
+				poster_path: "/33VdppGbeNxICrFUtW2WpGHvfYc.jpg",
+				id: 481848,
+				title: "The Call of the Wild",
+			},
+		],
+	},
+	{
+		paletteName: "Upcoming Movies",
+		id: "upcoming-movies",
+		emoji: "🎨",
+		movies: [
+			{
+				poster_path: "/f4aul3FyD3jv3v4bul1IrkWZvzq.jpg",
+				id: 508439,
+				title: "Onward",
+			},
+			{
+				poster_path: "/wxPhn4ef1EAo5njxwBkAEVrlJJG.jpg",
+				id: 514847,
+				title: "The Hunt",
+			},
+			{
+				poster_path: "/uCDcQbfIKY4oTPd6tbghloFm7Gi.jpg",
+				id: 606679,
+				title: "The High Note",
+			},
+			{
+				poster_path: "/A8FWpw3WHUo9EvU1xMWZRjdRlN3.jpg",
+				id: 647785,
+				title: "Debt Collectors",
+			},
+			{
+				poster_path: "/cjzU4g6SlScnP4MdkleyI25KGlR.jpg",
+				id: 582014,
+				title: "Promising Young Woman",
+			},
+			{
+				poster_path: "/v9wenXvoCF1LC5efdsJQOrBQQqU.jpg",
+				id: 524047,
+				title: "Greenland",
+			},
+			{
+				poster_path: "/shx7pGYQ9s4t7WFe19eES2WOrMS.jpg",
+				id: 610201,
+				title: "The Pale Door",
+			},
+			{
+				poster_path: "/5ud97JNxaWXQGqaOv7eddvCNra0.jpg",
+				id: 310583,
+				title: "Daddy's Girl",
+			},
+			{
+				poster_path: "/brR8YlcbYJ1Ig3g7QtoqJ65egcv.jpg",
+				id: 513584,
+				title: "Think Like a Dog",
+			},
+			{
+				poster_path: "/6VZlm8sEwxkE3L5nXxz17QLj1sF.jpg",
+				id: 471707,
+				title: "Corpus Christi",
+			},
+			{
+				poster_path: "/veZszwMZu8d3WMU6TJX9sV5w1Y4.jpg",
+				id: 470878,
+				title: "I Can Only Imagine",
+			},
+			{
+				poster_path: "/nKVrJm1dOwALdr2KHxNFhkbTSZc.jpg",
+				id: 517148,
+				title: "Beanpole",
+			},
+			{
+				poster_path: "/zQFjMmE3K9AX5QrBL1SXIxYQ9jz.jpg",
+				id: 579583,
+				title: "The King of Staten Island",
+			},
+			{
+				poster_path: "/sO2FECZGmmeisdYWzoQzGRjSZag.jpg",
+				id: 11257,
+				title: "A Room with a View",
+			},
+			{
+				poster_path: "/cdqZqIcWt0Ne2Io2OA9iWqqMuCA.jpg",
+				id: 589049,
+				title: "The Photograph",
+			},
+			{
+				poster_path: "/o1SoYYvxG4bWTXp4wNWt5sKQf1E.jpg",
+				id: 567965,
+				title: "Misbehaviour",
+			},
+			{
+				poster_path: "/5AJeDg97JlJxPEP38wDUlJ1OXgU.jpg",
+				id: 539531,
+				title: "It Must Be Heaven",
+			},
+			{
+				poster_path: "/eQJEXtDXpfEoDBRGkZ6LtPKacSl.jpg",
+				id: 516486,
+				title: "Greyhound",
+			},
+			{
+				poster_path: "/4ExrDZRhhmZkveXMjUzywc6266q.jpg",
+				id: 455957,
+				title: "Domino",
+			},
+			{
+				poster_path: "/gKwXP1VvKNxrc1400yksKA4D9Qc.jpg",
+				id: 617784,
+				title: "Military Wives",
+			},
+		],
+	},
+];
