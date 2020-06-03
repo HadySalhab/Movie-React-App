cc25dffda2cec5d80ffdd16565a708d217028a69
Hady Salhab <hadysalhab1@gmail.com>
2020-06-03 20:58:46 +1000

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import MoviePalette from "./components/Palette";
 import seedMovies from "./data/seedMovies";
 
@@ -6,9 +7,19 @@ import "./style/App.css";
 
 function App() {
 	return (
-		<div className="App">
-			<MoviePalette {...seedMovies[0]} />
-		</div>
+		<Switch>
+			<Route exact path="/" render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+			<Route
+				exact
+				path="/palette/:id"
+				render={() => <h1>Individual Palette</h1>}
+			/>
+			<Redirect to="/" />
+		</Switch>
+
+		// <div className="App">
+		// 	<MoviePalette {...seedMovies[0]} />
+		// </div>
 	);
 }
 
