1598303bd81c20f6a90eed18f585a53ad5581b2f
Hady Salhab <hadysalhab1@gmail.com>
2020-06-08 14:56:33 +1000

    Add Dialog to NewPalette

--- src/page/new-palette/components/MyToolbar.js
+++ src/page/new-palette/components/MyToolbar.js
@@ -4,9 +4,10 @@ import clsx from "clsx";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import MyDialog from "./MyDialog";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import PaletteNameForm from "./PaletteNameForm";
+
 import useStyles from "../style/MyToolbarStyle";
 
 function MyToolbar({ open, handleDrawerOpen, savePalette }) {
@@ -29,7 +30,8 @@ function MyToolbar({ open, handleDrawerOpen, savePalette }) {
 			<Typography variant="h3" noWrap>
 				New Movie Palette
 			</Typography>
-			<PaletteNameForm savePalette={savePalette} />
+			<MyDialog savePalette={savePalette} />
+
 			<Link to="/">
 				<Button variant="contained" color="secondary">
 					Go Back
