b98bb32fee1b1746be9403355ac61a84aa3c36b9
Hady Salhab <hadysalhab1@gmail.com>
2020-06-09 10:00:43 +1000

    Convert Navbar to functional component

--- src/page/palette/components/Navbar.js
+++ src/page/palette/components/Navbar.js
@@ -6,44 +6,35 @@ import Button from "@material-ui/core/Button";
 import { withStyles } from "@material-ui/styles";
 import styles from "../style/NavbarStyle";
 
-class Navbar extends Component {
-	constructor(props) {
-		super(props);
-		this.onSortChange = this.onSortChange.bind(this);
-	}
-	onSortChange(event) {
-		this.props.onSortChange(event.target.value);
-	}
-	render() {
-		const { title, sortBy, classes } = this.props;
-		return (
-			<nav className={classes.nav}>
-				<Link to="/">
-					<Button
-						className={classes.logo}
-						variant="contained"
-						color="secondary"
-					>
-						Movie Palette
-					</Button>
-				</Link>
-				<Select
-					classes={{
-						root: classes.selectRoot,
-						iconFilled: classes.iconFilled,
-					}}
-					variant="filled"
-					value={sortBy}
-					onChange={this.onSortChange}
-				>
-					<MenuItem value="original">Original</MenuItem>
-					<MenuItem value="popularity">By Popularity</MenuItem>
-					<MenuItem value="vote">By Vote</MenuItem>
-					<MenuItem value="date">By Date</MenuItem>
-					<MenuItem value="name">By Name</MenuItem>
-				</Select>
-			</nav>
-		);
-	}
-}
+const Navbar = (props) => {
+	const onSortChange = (event) => {
+		props.onSortChange(event.target.value);
+	};
+	const { title, sortBy, classes } = props;
+	return (
+		<nav className={classes.nav}>
+			<Link to="/">
+				<Button className={classes.logo} variant="contained" color="secondary">
+					Movie Palette
+				</Button>
+			</Link>
+			<Select
+				classes={{
+					root: classes.selectRoot,
+					iconFilled: classes.iconFilled,
+				}}
+				variant="filled"
+				value={sortBy}
+				onChange={onSortChange}
+			>
+				<MenuItem value="original">Original</MenuItem>
+				<MenuItem value="popularity">By Popularity</MenuItem>
+				<MenuItem value="vote">By Vote</MenuItem>
+				<MenuItem value="date">By Date</MenuItem>
+				<MenuItem value="name">By Name</MenuItem>
+			</Select>
+		</nav>
+	);
+};
+
 export default withStyles(styles)(Navbar);
