faeb77e9eee95092bfc0cacf16a888d82a286340
Hady Salhab <hadysalhab1@gmail.com>
2020-06-10 17:03:35 +1000

    Add Build

---
+++ build/precache-manifest.64ebf9b1e118c3ee372f3fb04b65c1cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a627a7b56ab3114c99463300ca72f663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90627a940c64aa95d25a",
+    "url": "/static/css/2.973e9ea1.chunk.css"
+  },
+  {
+    "revision": "3a1e4c12186994e0e29f",
+    "url": "/static/css/main.ae8e27ff.chunk.css"
+  },
+  {
+    "revision": "90627a940c64aa95d25a",
+    "url": "/static/js/2.3ad7cbc8.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.3ad7cbc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a1e4c12186994e0e29f",
+    "url": "/static/js/main.a1ac5174.chunk.js"
+  },
+  {
+    "revision": "b9fb15e88648c79db9e1",
+    "url": "/static/js/runtime-main.1222cf14.js"
+  }
+]);
